0bb7c1640c18fe43c4db0728c059879c0fea1d2f
AaronSledge <ahern2118@gmail.com>
2024-11-03 12:16:42 -0800

    Add books to populate homepage

--- public/script.js
+++ public/script.js
@@ -16,5 +16,32 @@ async function findBook() {
 }
 
 window.onload = async function loadTopBooks() {
-    
+    const items = document.querySelectorAll(".col");
+    try {
+        const response = await fetch("/api/search?q=stephen+king");
+        const data = await response.json();
+        
+        const books = data.docs;
+
+        Array.from(items).forEach((element, index) => {
+            if(index < books.length) {
+                const book = books[index];
+
+                const link = document.createElement('a');
+                link.href = `/results.html?bookKey=${encodeURIComponent(book.key)}`;
+                link.target = "_self";
+
+                const image = document.createElement("img");
+                image.src = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+                image.alt = book.title;
+
+                link.appendChild(image);
+                element.appendChild(link);
+            }
+            
+        });
+
+    } catch (error) {
+        console.error("Error fetching book data:", error);
+    }
 }
